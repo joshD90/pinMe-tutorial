4d5bae79fca409c3c7ed8f9f2f3ef5ffc3f25bcf
Joshua Dancey <joshuadancey@hotmail.com>
2023-01-13 20:43:05 +0000

    adding Google Oauth logic and starting css for login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import shareVideo from "../assets/share.mp4";
+import logowhite from "../assets/logowhite.png";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+          navigate("/");
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <GoogleOAuthProvider clientId="422095397036-ufl1tonilfkbtatjppkrqsip54n6tjae.apps.googleusercontent.com">
+      <Router>
+        <App />
+      </Router>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
