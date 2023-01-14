a8a5e018ff4ad0424945b5d791d95701afdee1bc
Joshua Dancey <joshuadancey@hotmail.com>
2023-01-14 22:29:09 +0000

    continuing to build out login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logowhite from "../assets/logowhite.png";
 
@@ -8,15 +9,33 @@ const Login = () => {
   const navigate = useNavigate();
   return (
     <div className="flex justify-start items-center flex-col h-screen">
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          console.log(credentialResponse);
-          navigate("/");
-        }}
-        onError={() => {
-          console.log("Login Failed");
-        }}
-      />
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          controls={false}
+          muted
+          autoPlay
+          Loop
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="p-5">
+          <img src={logowhite} width="130px" alt="logo" />
+        </div>
+        <div className="shadow-2xl">
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+              navigate("/");
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
